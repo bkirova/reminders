f924b843c47420245cd7792bc22b60ab5b751366
Borislava <b.kirova999@gmail.com>
2020-10-29 06:33:53 +0200

    more init files

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+
+<v-app>
+  <v-app-bar
+  app
+      absolute
+      color="#6A76AB"
+      dark
+      prominent
+      src="https://images.takeshape.io/1f1d0876-be74-4b33-99c8-6ac93f1d70db/dev/4468e4af-526e-4ed6-a0c1-a0dc7673cef3/nicolo-di-giovanni-535450-unsplash.png?auto=compress%2Cformat&w=1200"
+      scroll-target="#scrolling-techniques-4"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
+      <v-toolbar-title>Reminders</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-cog</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs v-model="tab" align-with-title>
+          <v-tab>Routine</v-tab>
+          <v-tab>Feed</v-tab>
+          <v-tab>Reminders</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-4"
+      class="overflow-y-auto"
+      max-height="800"
+    >
+      <v-container style="height: 1000px;margin-top:200px;" fluid>
+        <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <TodayBoard/>
+        </v-tab-item>
+        <v-tab-item>
+          <FeedBoard/>
+        </v-tab-item>
+        <v-tab-item>
+          <RemindersBoard/>
+        </v-tab-item>
+      </v-tabs-items>
+      </v-container>
+    </v-sheet>
+
+</v-app>
+</template>
+
+<script>
+import TodayBoard from './components/TodayBoard'
+import FeedBoard from './components/FeedBoard'
+import RemindersBoard from './components/RemindersBoard'
+
+export default {
+  name: 'App',
+
+  components: {
+    TodayBoard,
+    FeedBoard,
+    RemindersBoard
+  },
+
+  data: () => ({
+    tab: null
+  })
+}
+</script>
---
+++ src/components/RemindersBoard.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container>
+    <v-row v-for="notification in notifications" :key="notification.key" class="text-center">
+        <v-col class="mb-4">
+            <v-card
+                class="mx-auto"
+                max-width="400"
+                elevation="2"
+            >
+                <v-card-text class="headline font-weight-bold">
+                    {{notification.text}}
+                </v-card-text>
+
+                <v-divider class="mx-4"></v-divider>
+
+                <v-card-actions>
+                <v-list-item class="grow">
+                    <span class="subheading">Today at {{notification.time}}</span>
+                    <v-row
+                    align="center"
+                    justify="end"
+                    >
+                    <v-btn
+        color="deep-purple"
+        text
+        @click="edit"
+      >
+        Edit
+      </v-btn>
+                    </v-row>
+                </v-list-item>
+                </v-card-actions>
+            </v-card>
+        </v-col>
+</v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  name: 'ReminderBoard',
+
+  data: () => ({
+    notifications: [
+      {
+        text: 'Check Errors',
+        time: '09:00'
+      },
+      {
+        text: 'Check Jira tasks',
+        time: '09:30'
+      },
+      {
+        text: 'Check deploy schedules',
+        time: '10:00'
+      },
+      {
+        text: 'Close Deploy task',
+        time: '16:00'
+      }
+    ]
+  }),
+  methods: {
+    edit () {
+      console.log('edit')
+    }
+  }
+}
+</script>
